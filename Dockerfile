ff2a775bfcbe04a5038bf7fab0639efe65b1c2f7
Viktor <viktor.pulyak@pixelplex.io>
2019-11-27 22:50:22 +0300

    [SDC-75] Implement CI/CD for dev and stage

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM node:10.15
 WORKDIR /app
-ENV NODE_ENV="production"
 COPY package.json ./
 RUN NODE_ENV=development npm install
 COPY . .
--- src/config/config.interface.ts
+++ src/config/config.interface.ts
@@ -1,6 +1,5 @@
 export interface ConfigInterface {
   readonly MONGODB_URI: string;
   readonly MONGODB_DATABASE: string;
-  readonly AUTH_SECRET_KEY: string;
   readonly CORS: boolean;
 }
